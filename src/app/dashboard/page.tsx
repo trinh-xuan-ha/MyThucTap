29dfbe369b42f404497cb570fa7532718dd4cc04
trinh-xuan-ha <trinhxuanha8862@gmail.com>
2024-10-10 16:36:56 +0700

    add

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
-import UserInfo from "../../components/login/UserInfo"
-// import UserInfo from "@/components/login/userInfo"
-export default function Dashboard() {
-    return(
-        <UserInfo />
-    )
-}
+// import UserInfo from "../../components/login/UserInfo"
+// // import UserInfo from "@/components/login/userInfo"
+// export default function Dashboard() {
+//     return(
+//         <UserInfo />
+//     )
+// }
